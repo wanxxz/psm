aaf8e5eab8063e616b57eaacc1c6d6d7a21663fb
Yiwan Zhou <mail@yiwan.me>
2025-12-13 14:46:30 +0000

    wip

--- src/routes/sheet/index.tsx
+++ src/routes/sheet/index.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css'
 import { createFileRoute } from '@tanstack/solid-router'
 import { createEffect, createResource, createSignal, Show, Suspense } from 'solid-js'
 
@@ -10,7 +11,7 @@ function SheetComponent(props: { module: { default: Function } }) {
     ref?.appendChild(el)
   })
 
-  return <div ref={ref}></div>
+  return <div class={css({ position: 'absolute', width: '100%', height: '100%' })} ref={ref}></div>
 }
 
 function SheetComponentSuepense() {
